55038d2cbd28d727737ac608ca3dad929831bd75
anwbrcl <honorofpaagrio@outlook.com>
2017-10-02 10:30:25 +0300

    lab commit

---
+++ lab2/lab2+/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTest
+{
+    public class Calculator
+    {        
+        public static double Addition(double first, double second)
+        {
+            return first + second;  
+        }
+        public static double Subtraction(double first, double second)
+        {
+            return first - second;
+        }
+        public static double Multiplication(double first, double second)
+        {
+            return first * second;
+        }
+        public static double Division(double first, double second)
+        {
+            return first / second;
+        }
+    }
+}
---
+++ lab2/lab2+/TestMethods.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+
+namespace AppTest
+{
+    [TestFixture]
+    public class TestMethods
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            double result = Calculator.Addition(1, 1);
+            Assert.AreEqual(2, result);
+            result = Calculator.Addition(1, -1);
+            Assert.AreEqual(0, result);
+            result = Calculator.Addition(-1, 1);
+            Assert.AreEqual(0, result);
+            result = Calculator.Addition(-1, -1);
+            Assert.AreEqual(-2, result);
+        }
+
+        [Test]
+        public void SubtractionTest()
+        {
+            double result = Calculator.Subtraction(1, 1);
+            Assert.AreEqual(0, result);
+            result = Calculator.Subtraction(1, -1);
+            Assert.AreEqual(2, result);
+            result = Calculator.Subtraction(-1, 1);
+            Assert.AreEqual(-2, result);
+            result = Calculator.Subtraction(-1, -1);
+            Assert.AreEqual(0, result);
+            result = Calculator.Subtraction(-1, 1);
+            Assert.AreEqual(-2, result);
+        }
+        [Test]
+        public void MultiplicationTest()
+        {
+            double result = Calculator.Multiplication(1, 1);
+            Assert.AreEqual(1, result);
+            result = Calculator.Multiplication(1, -1);
+            Assert.AreEqual(-1, result);
+            result = Calculator.Multiplication(-1, 1);
+            Assert.AreEqual(-1, result);
+            result = Calculator.Multiplication(-1, -1);
+            Assert.AreEqual(1, result);
+            result = Calculator.Multiplication(-1, 1);
+            Assert.AreEqual(-1, result);
+        }
+        [Test]
+        public void DivisionTest()
+        {
+            double result = Calculator.Division(1, 1);
+            Assert.AreEqual(1, result);
+            result = Calculator.Division(1, -1);
+            Assert.AreEqual(-1, result);
+            result = Calculator.Division(-1, 1);
+            Assert.AreEqual(-1, result);
+            result = Calculator.Division(-1, -1);
+            Assert.AreEqual(1, result);
+            result = Calculator.Division(-1, 1);
+            Assert.AreEqual(-1, result);
+        }
+
+    }
+}
