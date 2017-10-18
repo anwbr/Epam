6173b77d436e233e390b913aeb2bd6f7e98f1c0a
anwbrcl <honorofpaagrio@outlook.com>
2017-10-19 01:28:30 +0300

    lab3 selenium

---
+++ lab3/lab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab3")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8834cee0-3d65-47e9-a351-e8bf0e583a07")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab3/lab3/SeleniumExampleTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Chrome;
+
+namespace lab3
+{
+    [TestClass]
+    public class SeleniumExampleTest
+    {
+        private static IWebDriver webDriverChrome;
+        private const string username = "Kreker";
+        private const string password = "Time4Death!";
+        [AssemblyInitialize]
+        public static void SetUP(TestContext testContext)
+        {
+            try
+            {
+                webDriverChrome = new ChromeDriver(@"C:\Program Files\ChromeGeckoDrivers\");
+            }
+            catch (Exception exception)
+            {
+                exception.StackTrace.ToString();
+            }
+            
+        }
+        [TestMethod]
+        public void TestChrome()
+        {
+            webDriverChrome.Navigate().GoToUrl("https://vk.com/");
+            webDriverChrome.FindElement(By.Id("index_email")).SendKeys(username);
+            webDriverChrome.FindElement(By.Id("index_pass")).SendKeys(password);
+            webDriverChrome.FindElement(By.XPath("//button[@id='index_login_button']")).Click();
+            System.Threading.Thread.Sleep(3000);
+        }
+        [TestCleanup]
+        public void TestCloseChrome()
+        {
+            if (webDriverChrome != null)
+                webDriverChrome.Quit();
+        }
+    }
+}
